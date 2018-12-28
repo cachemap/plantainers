d3e7f6c4b004a55f67ffcab0f3c4db1e13ea1fe9
Dillon <dillon.notz@gmail.com>
2018-12-28 00:05:25 -0500

    Create setup file for remote_server; (add install python3, aiohttp)

---
+++ setup_remote_server.sh
@@ -0,0 +1,18 @@
+i# Ensure bash is installed
+if [ python3 -v python3 &>/dev/null ] then
+    echo "Python 3 is installed"
+else
+    # Install Python 3 for Linux (TODO: Switch for operating system)
+    sudo apt-get install python3.7
+fi
+
+# Setup virtual environment
+python3 -m venv tutorial-env
+
+# Switch to virtual environment
+source tutorial-env/bin/activate
+
+# Install Python modules in virtual environment
+pip install aiohttp  # Asynchronous I/O for GPIO control 
+pip install aiodns   # Faster DNS resolution for aiohttp
+pip install 
