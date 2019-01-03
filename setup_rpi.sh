e2b3ea27cbf39f485876e4e274a9a4dd1ec7fa0d
Dillon <dillon.notz@gmail.com>
2019-01-03 18:59:26 -0500

    Create new Raspberry Pi device setup

---
+++ setup_rpi.sh
@@ -0,0 +1,19 @@
+##
+# Update device's system package list
+##
+
+echo "Updating device's system package list..."
+sudo apt-get update
+sudo apt-get dist-upgrade # Upgrade all installed packages to their latest versions
+
+
+##
+# Install useful system packages
+##
+
+# Download and install node.js
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Check node was installed correctly
+# if [ node -v eq 8.x ] # TODO: Fix this stuff 
